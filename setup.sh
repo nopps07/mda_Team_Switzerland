3a6a98ff20abc75ad03194f7529a8b260580d967
Gunho Lee <49727675+nopps07@users.noreply.github.com>
2022-06-04 02:30:35 +0200

    June 4: Heroku

---
+++ setup.sh
@@ -0,0 +1,12 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"gratefulnessofsmile@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
